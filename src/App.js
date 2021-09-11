c321274475c64c4ee71071eb474c1a0fb512582f
pavancrazy007 <vinukondapavankumar1@gmail.com>
2021-09-11 14:36:25 +0000

    worksfine

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {Component} from 'react'
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    isLoggedIn: true,
+  }
+
+  render() {
+    const {isLoggedIn} = this.state
+
+    return (
+      <div className="container">
+        <Welcome />
+        {isLoggedIn ? (
+          <button type="button">Logout</button>
+        ) : (
+          <button type="button">Login</button>
+        )}
+      </div>
+    )
+  }
+}
+
+export default App
